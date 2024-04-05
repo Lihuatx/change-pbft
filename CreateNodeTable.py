0de17f16824357a427c3248d9924cc9d24108aab
Lihuatx <1902561088@qq.com>
2024-04-05 16:05:30 +0800

    增加调试函数

--- CreateNodeTable.py
+++ CreateNodeTable.py
@@ -1,4 +1,4 @@
-clusters = ['N', 'M', 'P']  # 集群的标识符
+clusters = ['N']  # 集群的标识符
 nodes_per_cluster = 26  # 每个集群的节点数
 base_port = 1110  # 基础端口号
 
@@ -12,7 +12,7 @@ node_table = {}
 # 对于每个 IP 地址，创建一组节点
 for ip_index, ip_address in enumerate(ip_addresses):
     # 计算当前 IP 地址对应的集群标识符
-    cluster = clusters[0]
+    cluster = clusters[ip_index % len(clusters)]
 
     # 确保 node_table 中有对应的集群键
     if cluster not in node_table:
