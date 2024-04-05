1f7dd4a9d80020f5e3772c973e4003072c6e6765
Lihuatx <1902561088@qq.com>
2024-04-05 16:04:48 +0800

    增加调试函数

--- CreateNodeTable.py
+++ CreateNodeTable.py
@@ -12,7 +12,7 @@ node_table = {}
 # 对于每个 IP 地址，创建一组节点
 for ip_index, ip_address in enumerate(ip_addresses):
     # 计算当前 IP 地址对应的集群标识符
-    cluster = clusters[ip_index % len(clusters)]
+    cluster = clusters[0]
 
     # 确保 node_table 中有对应的集群键
     if cluster not in node_table:
