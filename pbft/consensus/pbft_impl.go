4bb3cc336616960dfb58f28f4ae6b89a3d019b2c
Lihuatx <1902561088@qq.com>
2024-04-05 16:32:57 +0800

    增加调试函数

--- pbft/consensus/pbft_impl.go
+++ pbft/consensus/pbft_impl.go
@@ -33,7 +33,7 @@ const (
 // f: # of Byzantine faulty node
 // f = (n­1) / 3
 // n = 4, in this case.
-const f = 18
+const f = 8
 
 // lastSequenceID will be -1 if there is no last sequence ID.
 func CreateState(viewID int64, lastSequenceID int64) *State {
--- sendmes.py
+++ sendmes.py
@@ -4,12 +4,12 @@ import datetime
 
 # 在新的PowerShell窗口中执行命令
 print(datetime.datetime.now())
-for i in range(200):
+for i in range(1):
     # 动态构建带有当前循环i值的PowerShell命令
     ps_command = f"""
     $headers = @{{ "Content-Type" = "application/json" }}
     $body = '{{"clientID":"ahnhwi","operation":"GetMyName","timestamp":{i}}}'
-    $response = Invoke-WebRequest -Uri "http://localhost:1111/req" -Method POST -Headers $headers -Body $body
+    $response = Invoke-WebRequest -Uri "http://114.55.130.178:1110/req" -Method POST -Headers $headers -Body $body
     """
 
     subprocess.Popen(['powershell', '-Command', ps_command])
