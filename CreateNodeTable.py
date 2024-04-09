4fb3593903ddbf78045c0e99dfe0ee93ccea815a
Lihuatx <1902561088@qq.com>
2024-04-09 10:46:37 +0800

    add test.sh

--- CreateNodeTable.py
+++ CreateNodeTable.py
@@ -1,27 +1,25 @@
 import sys
 
-clusters = ['N', 'N', 'N']  # 集群的标识符
 arg = sys.argv[1]
-nodes_per_cluster = int(arg)  # 每个集群的节点数量
+nodes_per_cluster = int(arg)  # 节点总数
 client = sys.argv[2]
 server = sys.argv[3]
 base_port = 1110  # 基础端口号
 
 # 计算前三分之一节点的数量（向上取整）
-first_third = -(-nodes_per_cluster // 3)  # 使用双负号进行向上取整
+first_third = (nodes_per_cluster + 2) // 3  # 使用整数除法并向上取整
 
 # 初始化 NodeTable
 node_table = {}
-for cluster in clusters:
-    for i in range(nodes_per_cluster):
-        node_id = f"{cluster}{i}"
-        if i < first_third:
-            # 前三分之一的节点使用 client IP 地址
-            address = f"{client}:{base_port + i}"
-        else:
-            # 后三分之二的节点使用 server IP 地址
-            address = f"{server}:{base_port + i}"
-        node_table[node_id] = address
+for i in range(nodes_per_cluster):
+    node_id = f"N{i}"
+    if i < first_third:
+        # 前三分之一的节点使用 client IP 地址
+        address = f"{client}:{base_port + i}"
+    else:
+        # 后三分之二的节点使用 server IP 地址
+        address = f"{server}:{base_port + i}"
+    node_table[node_id] = address
 
 # 将 NodeTable 保存到 nodetable.txt 文件中
 with open('nodetable.txt', 'w') as file:
