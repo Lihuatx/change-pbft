7255aa25ccab4fb7e623f2e9805473cc60a9ce5f
Lihuatx <1902561088@qq.com>
2024-05-12 16:35:50 +0800

    增加5台服务器的测试脚本

--- linuxTest.py
+++ linuxTest.py
@@ -11,6 +11,6 @@ if arg == "N0":
     command = f"./app.exe client N"
     subprocess.run(['tmux', 'new-window', '-t', f'myClient:{1}', '-n', "Client-1"])
 
-    tmux_command = f"tmux send-keys -t myClient:{1} './app client N' C-m"
+    tmux_command = f"tmux send-keys -t myClient:{1} './app.exe client N' C-m"
 
     subprocess.Popen(['bash', '-c', tmux_command])
