08ce034ab83385bd786a626d438a1b60775a5237
Lihuatx <1902561088@qq.com>
2024-05-12 16:34:43 +0800

    增加5台服务器的测试脚本

--- linuxTest.py
+++ linuxTest.py
@@ -8,7 +8,7 @@ if arg == "N0":
     subprocess.run(['tmux', 'kill-session', '-t', 'myClient'])
     subprocess.run(['tmux', 'new-session', '-d', '-s', 'myClient'])
 
-    command = f"./app client N"
+    command = f"./app.exe client N"
     subprocess.run(['tmux', 'new-window', '-t', f'myClient:{1}', '-n', "Client-1"])
 
     tmux_command = f"tmux send-keys -t myClient:{1} './app client N' C-m"
